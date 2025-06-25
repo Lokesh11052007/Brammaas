04d3e2259f6b16710a1e22859eb8554a8b4d918d
Lokeshwaran R <lokesh11052007@gmail.com>
2025-06-25 17:15:34 +0530

    related

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,11 @@
 import HeroSection from "./herosection.jsx"
 import AboutSchool from "./about.jsx"
-import  AcademicCarousel from './academics.jsx'
 import WhyUs from "./whyus.jsx"
 import ParentsTestimonials from "./parent.jsx"
 import Toppers from "./Topper.jsx"
 import Gallery from "./gallery.jsx"
 import BackgroundSection from "./support.jsx"
-
+import AcademicCarousel from '../academic/academic1/RelatedCourse.jsx'
 
 export default function Home() {
     return ( 
